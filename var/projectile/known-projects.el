a89b4f52f59edc0a02ede15e8a14ab47cfbdefdf
Aidan Bailey <BLYAID001@myuct.ac.za>
2020-05-24 14:22:49 +0200

    deleted old files

--- var/projectile/known-projects.el
+++
@@ -1,1 +0,0 @@
-("~/.emacs.d/" "~/Academic/CSC3022H/Handins/Assignments/Assignment6/" "~/Projects/Software/python/pycard/" "~/Academic/CSC3002F/OS/Assignment1/" "~/Academic/CSC3022H/Handins/Assignments/Assignment5/" "~/Academic/CSC3022H/Handins/Assignments/Assignment4/" "~/Academic/CSC3022H/Handins/Assignments/Assignment3/" "~/.emacs.d.bak/")
